18dcf4d6f9d1e2e63362f990a0685de143548068
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-01-12 14:19:39 -0300

    feat: add repositories page and route

---
+++ src/pages/RepositoriesPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const RepositoriesPage = () => {
+  return (
+    <Container>
+      <h1>Repositories</h1>
+    </Container>
+  );
+};
+
+export default RepositoriesPage;
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MainPage from '../pages/MainPage';
+import RepositoriesPage from '../pages/RepositoriesPage';
 
 const AppRoutes: React.FC = () => (
   <Routes>
     <Route path="/" element={<MainPage />} />
+    <Route path="/repositories" element={<RepositoriesPage />} />
   </Routes>
 );
 
