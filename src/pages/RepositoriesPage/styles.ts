e691b5eabaa179263f6c983ad63be32b32aaff63
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-01-13 15:15:07 -0300

    style: margin adjustment for user image

--- src/pages/RepositoriesPage/styles.ts
+++ src/pages/RepositoriesPage/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.main`
 
 export const Sidebar = styled.aside`
   background: ${(props) => props.theme.defaultColors.background};
-  min-width: 8rem;
+  min-width: 20rem;
   max-height: 100vh;
   overflow-y: hidden;
 `;
