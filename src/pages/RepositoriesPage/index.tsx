c70798944d19bf2d947510f675b57f0a6c7f6af7
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-01-14 14:25:11 -0300

    chore: using repositories on page

--- src/pages/RepositoriesPage/index.tsx
+++ src/pages/RepositoriesPage/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 import { Container, Sidebar, Main } from './styles';
 import Profile from '../../components/Profile';
+import Filter from '../../components/Filter';
+import Repositories from '../../components/Repositories';
 
 const RepositoriesPage = () => {
   return (
     <Container>
       <Sidebar>
         <Profile />
+        <Filter />
       </Sidebar>
-      <Main>Main</Main>
+      <Main>
+        <Repositories />
+      </Main>
     </Container>
   );
 };
