a7824ea16e9d274bf84947befdb69ebab954c661
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-01-14 14:20:39 -0300

    style: add style filter component

---
+++ src/components/Filter/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  gap: 0.2rem;
+  margin: 1rem auto;
+  padding-right: 2rem;
+
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.sm}) {
+    grid-template-columns: auto auto;
+    gap: 0.8rem;
+    padding: 0 1rem;
+  }
+`;
+
+export const Selector = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: ${(props) => props.theme.defaultColors.container};
+  color: ${(props) => props.color || props.theme.defaultColors.text};
+  width: 100%;
+  min-height: 2rem;
+  border: none;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  transition: background 0.3s, transform 0.3s;
+  padding: 0 1rem;
+
+  &:hover,
+  &.selected {
+    background: ${(props) => props.color || props.theme.defaultColors.light};
+    color: ${(props) => (props.color ? props.theme.assortedColors.white : props.theme.assortedColors.black)};
+    transform: translateX(5px) scale(1.02);
+  }
+
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.sm}) {
+    border-radius: 20px;
+    &:hover,
+    &.selected {
+      transform: translateX(0) scale(1.02);
+    }
+  }
+`;
+
+export const Cleaner = styled.button`
+  background: transparent;
+  border: none;
+  color: ${(props) => props.theme.defaultColors.text};
+  text-align: left;
+  padding: 1rem;
+
+  &:hover {
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.sm}) {
+    text-align: center;
+    padding: 0 1rem;
+  }
+`;
