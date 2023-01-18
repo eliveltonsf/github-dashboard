40e182f00b764b6f5d365b9314d778b949b584ab
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-01-18 14:23:51 -0300

    chore: adjust api function typing

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,8 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
-export const getUser = async (login: string) => api.get(`/users/${login}`);
+export const getUser = async (login: string | undefined) => api.get(`/users/${login}`);
 
-export const getRepos = async (login: string) => api.get(`/users/${login}/repos`);
+export const getRepos = async (login: string | undefined) => api.get(`/users/${login}/repos`);
 
 export default api;
